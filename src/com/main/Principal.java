a421217675f07875ed9d6ee9b89c4e0cf39e6ed7
Hardcorder <hardcorder@hotmail.com>
2015-07-20 12:16:03 +0100

    New Comments

--- src/com/main/Principal.java
+++ src/com/main/Principal.java
@@ -9,7 +9,7 @@ public class Principal
 	private static int portaTs = 9987; // Normally this is the first teamspeak port
 	private static String usernameTs = "Username"; // Guest SErver Query Login
 	private static String passwordTs = "Password"; // guest Server Query Password
-	private static String icecastStream = "http://ShoutCastIp:ShoutCastPort/currentsong?sid=1";
+	private static String icecastStream = "http://ShoutCastIp:ShoutCastPort/currentsong?sid=StreamIdNormallyIs1";
 
 	public static void main(String[] args) 
 	{
