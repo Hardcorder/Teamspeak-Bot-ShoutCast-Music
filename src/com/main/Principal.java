54d4d78f5792dbfe9e2db210f1d5a68dd63ab437
Hardcorder <hardcorder@hotmail.com>
2015-07-20 11:49:21 +0100

    New

--- src/com/main/Principal.java
+++ src/com/main/Principal.java
@@ -4,25 +4,18 @@ import com.bot.*;
 
 public class Principal 
 {
-	private static String ipTs = "ts.htlv-team.com";
-	private static int portaTsQuery = 10011;
-	private static int portaTs = 9987;
-	private static String usernameTs = "Teste";
-	private static String passwordTs = "Q6XH58ZL";
-	private static String icecastStream = "http://188.93.234.224:8005/currentsong?sid=1";
+	private static String ipTs = "Teamspeak Ip Adress";
+	private static int portaTsQuery = 10011; // Port of Server Query
+	private static int portaTs = 9987; // Normally this is the first teamspeak port
+	private static String usernameTs = "Teste"; // Guest SErver Query Login
+	private static String passwordTs = "Q6XH58ZL"; // guest Server Query Password
+	private static String icecastStream = "http://ShoutCastIp:ShoutCastPort/currentsong?sid=1";
 
 	public static void main(String[] args) 
 	{
-		/*
-		Bot bot = new Bot("ts.htlv-team.com", 10011, 9987, "Admin", "9RsrXtH4");
-		bot.conectar();
-		bot.setNome("Htlv1");
-		bot.enviaMensagemSala("Teste htlv-team.com bot");
-		bot.iniciaEventos();
-		*/
 		BotShoutCastMusicBroadcast botShoutCast = new BotShoutCastMusicBroadcast(ipTs, portaTsQuery, portaTs, usernameTs, passwordTs, icecastStream);
 		botShoutCast.conectar();
-		botShoutCast.setNome("Htlv-team.com Radio");
+		botShoutCast.setNome("Bot Name[Htlv-team.com]");
 		botShoutCast.iniciaEventos();
 		botShoutCast.run();
 	}
